1fdca71f182d7195b2431fa8bd8bfa3d8a4ccdbd
shvinius <shvinius@inbox.lt>
2022-05-30 16:00:11 +0300

    Padaryta antra uzduotis su mokytojais kuriuos reikejo nuskaityti ir isvesti i konsole

---
+++ AntraUzduotisMokytojai/Mokytojai.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntraUzduotisMokytojai
+{
+    internal class Mokytojai
+    {
+        private string vardas;
+        private string pavarde;
+        private string klase;
+        private string pareigos;
+        private int stazas;
+        private string issilavinimas;
+
+        public Mokytojai(string vardas, string pavarde, string klase, string pareigos, int stazas, string issilavinimas)
+        {
+            this.vardas = vardas;
+            this.pavarde = pavarde;
+            this.klase = klase;
+            this.pareigos = pareigos;
+            this.stazas = stazas;
+            this.issilavinimas = issilavinimas;
+        }
+
+        public Mokytojai()
+        {
+        }
+
+        public string Vardas
+        {
+            get { return vardas; }
+            set { vardas = value; }
+        }
+        public string Pavarde
+        {
+            get { return pavarde; }
+            set { pavarde = value; }
+        }
+        public string Klase
+        {
+            get { return klase; }
+            set { klase = value; }
+        }
+
+        public string Pareigos
+        {
+            get { return pareigos; }
+            set { pareigos = value; }
+        }
+        public int Stazas
+        {
+            get { return stazas; }
+            set { stazas = value; }
+        }
+        public string Issilavinimas
+        {
+            get { return issilavinimas; }
+            set { issilavinimas = value; }
+        }
+    }
+}
---
+++ AntraUzduotisMokytojai/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AntraUzduotisMokytojai
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mokytojai teacher1;
+
+            teacher1 = NuskaitytiMokytoja();
+
+            Console.WriteLine("----------------------------");
+
+            IsvestiMokytojus(teacher1);
+        }
+        static Mokytojai NuskaitytiMokytoja()
+        {
+            Mokytojai teacher = new Mokytojai();
+
+            Console.WriteLine("Įveskite mokytojo vardą: ");
+            string vardas = Console.ReadLine();
+            teacher.Vardas = vardas;
+            
+
+           
+            Console.WriteLine("Įveskite mokytojo pavardę: ");
+            string pavarde = Console.ReadLine();
+            teacher.Pavarde = pavarde;
+           
+
+            Console.WriteLine("Įveskite mokytojo auklėtinių klasę: ");
+            string klase = Console.ReadLine();
+            teacher.Klase = klase;
+            
+
+            Console.WriteLine("Įveskite mokytojo pareigas: ");
+            string pareigos = Console.ReadLine();
+            teacher.Pareigos = pareigos;
+
+            Console.WriteLine("Įveskite mokytojo stažą: ");
+            int stazas = Convert.ToInt32(Console.ReadLine());
+            teacher.Stazas = stazas;
+
+            Console.WriteLine("Įveskite mokytojo išsilavinimas: ");
+            string issilavinimas = Console.ReadLine();
+            teacher.Issilavinimas = issilavinimas;
+
+
+            return teacher;
+        }
+        static void IsvestiMokytojus(Mokytojai teacher)
+        {
+            Console.WriteLine("Vardas: " + teacher.Vardas);
+            Console.WriteLine("Pavardė: " + teacher.Pavarde);
+            Console.WriteLine("Mokytojo auklėtinių klasė: " + teacher.Klase);
+            Console.WriteLine("Pareigos: " + teacher.Pareigos);
+            Console.WriteLine("Stažas: " + teacher.Stazas + "m.");
+            Console.WriteLine("Išsilavinimas: " + teacher.Issilavinimas);
+        }
+    }
+}
